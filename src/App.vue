f03f10ae37d356ca7134a75099d968951cbc7b39
Christian Püschel <christian.code1296@gmail.com>
2025-10-26 11:10:37 +0100

    Welcome Page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 
+import Welcome from "./components/pages/Welcome.vue";
+import Layout from "./components/layouts/Layout.vue";
+import Dashboard from "./components/pages/Dashboard.vue";
 </script>
 
 <template>
-
+  <Layout>
+    <Welcome />
+    <Dashboard />
+  </Layout>
 </template>
 
 <style scoped>
--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -3,9 +3,60 @@
 </script>
 
 <template>
-
+  <header>
+  <h1 class="text-gradient">SMOLGRAM </h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+  <footer>
+  <small>Created by Christian "Ace"</small>
+    <a href="https://github.com/christiancode1296/FitnessApp" target="_blank">
+      <img alt="pfp" src="https://avatars.githubusercontent.com/u/143355041?v=4"/>
+      <p>Ace</p>
+    </a>
+  </footer>
 </template>
 
 <style scoped>
+  header, footer, main {
+    padding: 1rem;
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+
+
+  main {
+    flex: 1;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    align-items: center;
+    padding: 2rem 0 3rem;
+
+  }
+  footer a {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 0.3rem 0.5rem 0.3rem 0.3rem;
+    background: var(--background-muted);
+    border-radius: 4rem;
+    border: 1px solid transparent;
+    transition-duration: 200ms;
+    text-decoration: none;
+  }
+  footer a:hover {
+    border-color: var(--color-link);
+  }
+  footer a img {
+    max-width: 30px;
+    aspect-ratio: 1/1;
+    border-radius: 100%;
+  }
 
 </style>
